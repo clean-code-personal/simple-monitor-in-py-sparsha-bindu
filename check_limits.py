494fbaf832fa6f76851b15fed7512a9fd24c58bd
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-14 19:53:18 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,32 +1,15 @@
-def check_temperature(temperature):
-  if temperature < 0:
+def check_vital(vital_name, value, min_value, max_value):
+  if value < min_value:
     return False, 'low'
-  elif temperature > 45:
+  elif value > max_value:
     return False, 'high'
   else:
     return True, None
-
-def check_soc(soc):
-  if soc < 20:
-    return False, 'low'
-  elif soc > 80:
-    return False, 'high'
-  else:
-    return True, None
-
-def check_charge_rate_ok(charge_rate):
-  if charge_rate > 0.8:
-    return False, 'high'
-  else:
-    return True, None
-
-def print_error_message(vital_name, breach_type):
-  print('{} is {}!'.format(vital_name, breach_type))
 
 def is_battery_ok(temperature, soc, charge_rate):
-  is_temp_ok, temp_breach_type = check_temperature(temperature)
-  is_soc_ok, soc_breach_type = check_soc(soc)
-  is_charge_rate_ok, charge_breach_type = check_charge_rate_ok(charge_rate)
+  is_temp_ok, temp_breach_type = check_vital('temperature', temperature, 0, 45)
+  is_soc_ok, soc_breach_type = check_vital('state of charge', soc, 20, 80)
+  is_charge_rate_ok, charge_breach_type = check_vital('charge rate', charge_rate, 0, 0.8)
   
   if not is_temp_ok:
     return False, 'temperature', temp_breach_type
