3184b35fcdf427514673fe2d7d55da05a4021d19
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-14 21:58:10 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -24,32 +24,12 @@ def print_error_message(vital_name, breach_type):
   print('{} is {}!'.format(vital_name, breach_type))
   
 def is_battery_ok(temperature, soc, charge_rate):
-  ok = True
-  vital_name = None
-  breach_type = None
+    checks = {'temperature': check_temperature(temperature),
+              'state of charge': check_soc(soc),
+              'charge rate': check_charge_rate_ok(charge_rate)}
+    failed = [(vital_name, breach_type) for vital_name, (is_ok, breach_type) in checks.items() if not is_ok]
+    return (not failed, failed[0][0] if failed else None, failed[0][1] if failed else None)
 
-  # check temperature
-  is_ok, breach_type = check_temperature(temperature)
-  if not is_ok:
-    ok = False
-    vital_name = 'temperature'
-
-  # check state of charge
-  is_ok, breach_type = check_soc(soc)
-  if not is_ok:
-    ok = False
-    vital_name = 'state of charge'
-
-  # check charge rate
-  is_ok, breach_type = check_charge_rate_ok(charge_rate)
-  if not is_ok:
-    ok = False
-    vital_name = 'charge rate'
-
-  if not ok:
-    return False, vital_name, breach_type
-  else:
-    return True, None, None
 
 def battery_status(temperature, soc, charge_rate, reporter=print_error_message):
   is_ok, vital_name, breach_type = is_battery_ok(temperature, soc, charge_rate)
@@ -60,7 +40,7 @@ def battery_status(temperature, soc, charge_rate, reporter=print_error_message):
 if __name__ == '__main__':
     assert(battery_status(25, 70, 0.7) == (True, None, None))
     assert(battery_status(-5, 70, 0.7) == (False, 'temperature', 'low'))
-    assert(battery_status(50, 70, 0.7) == (False, 'temperature', 'low'))
+    assert(battery_status(50, 70, 0.7) == (False, 'temperature', 'high'))
     assert(battery_status(25, 10, 0.7) == (False, 'state of charge', 'low'))
-    assert(battery_status(25, 90, 0.7) == (False, 'state of charge', 'low'))
+    assert(battery_status(25, 90, 0.7) == (False, 'state of charge', 'high'))
     assert(battery_status(25, 70, 0.9) == (False, 'charge rate', 'high'))
