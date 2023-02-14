00d33f0d1cc8a7231c74600481feb62aeaa52506
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-14 20:46:44 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -24,16 +24,30 @@ def print_error_message(vital_name, breach_type):
   print('{} is {}!'.format(vital_name, breach_type))
 
 def is_battery_ok(temperature, soc, charge_rate):
-  is_temp_ok, temp_breach_type = check_temperature(temperature)
-  is_soc_ok, soc_breach_type = check_soc(soc)
-  is_charge_rate_ok, charge_breach_type = check_charge_rate_ok(charge_rate)
-  
-  if not is_temp_ok:
-    return False, 'temperature', temp_breach_type
-  elif not is_soc_ok:
-    return False, 'state of charge', soc_breach_type
-  elif not is_charge_rate_ok:
-    return False, 'charge rate', charge_breach_type
+  ok = True
+  vital_name = None
+  breach_type = None
+
+  # check temperature
+  is_ok, breach_type = check_temperature(temperature)
+  if not is_ok:
+    ok = False
+    vital_name = 'temperature'
+
+  # check state of charge
+  is_ok, breach_type = check_soc(soc)
+  if not is_ok:
+    ok = False
+    vital_name = 'state of charge'
+
+  # check charge rate
+  is_ok, breach_type = check_charge_rate_ok(charge_rate)
+  if not is_ok:
+    ok = False
+    vital_name = 'charge rate'
+
+  if not ok:
+    return False, vital_name, breach_type
   else:
     return True, None, None
 
