eee9d2cf7af9edf836beea270e82d212b0507b7b
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-19 20:47:15 +0530

    Update test_check_limits.py

--- test_check_limits.py
+++ test_check_limits.py
@@ -1,38 +1,14 @@
+
 import unittest
-from check_limits import Battery, check_battery
+from check_limits import Battery,check_battery
 
-class TestBattery(unittest.TestCase):
-    def test_battery_ok(self):
-        battery = Battery(25, 70, 0.7)
-        self.assertFalse(check_battery(battery))
+class TestBatteryIsOk(unittest.TestCase):
+    def test_battery_is_ok(self):
+        self.assertTrue(check_battery(Battery(25, 70, 0.7, "Celcius",'en')))
+        self.assertFalse(check_battery(Battery(-5, 60, 0.6, "Celcius",'ge')))
+        self.assertFalse(check_battery(Battery(104, 90, 0.7, "Fahrenheit",'kn')))
+        self.assertFalse(check_battery(Battery(86, 75, 0.9, "Fahrenheit",'fr')))
 
-    def test_battery_temperature_low(self):
-        battery = Battery(-5, 70, 0.7)
-        self.assertFalse(check_battery(battery))
-    
-    def test_battery_temperature_high_soc_high(self):
-        battery = Battery(50, 85, 0)
-        self.assertFalse(check_battery(battery))
-
-    def test_battery_soc_low_charge_rate_high(self):
-        battery = Battery(0, 20, 0.8)
-        self.assertFalse(check_battery(battery))
-
-    def test_battery_soc_high_charge_rate_high(self):
-        battery = Battery(45, 80, 0.8)
-        self.assertFalse(check_battery(battery))
-
-    def test_battery_temperature_low_soc_low_charge_rate_low(self):
-        battery = Battery(-1, 19, -1)
-        self.assertFalse(check_battery(battery))
-
-    def test_battery_temperature_high_soc_high_charge_rate_high(self):
-        battery = Battery(46, 81, 0.9)
-        self.assertFalse(check_battery(battery))
-
-    def test_battery_temperature_ok_soc_ok_charge_rate_ok(self):
-        battery = Battery(22, 60, 0.4)
-        self.assertFalse(check_battery(battery))
 
 if __name__ == '__main__':
     unittest.main()
