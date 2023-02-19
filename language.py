4adee9f5e528f3cfa5b8c105cb7dc95a3a41e0a2
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-02-19 20:47:44 +0530

    Create language.py

---
+++ language.py
@@ -0,0 +1,41 @@
+messages = {
+    'en': {
+        'outOfRange': lambda breach_type: f'{breach_type} is out of range!',
+        'temperature': 'Temperature',
+        'soc': 'State of Charge',
+        'chargeRate': 'Charge rate',
+        'batteryOk': 'Battery is OK',
+        'batteryNotOk': 'Battery is not OK'
+    },
+    'ge': {
+        'outOfRange': lambda breach_type: f'{breach_type} ist außerhalb des Bereichs!',
+        'temperature': 'Temperatur',
+        'soc': 'Ladezustand',
+        'chargeRate': 'Ladegeschwindigkeit',
+        'batteryOk': 'Batterie ist okay',
+        'batteryNotOk': 'Batterie ist nicht okay'
+    },
+    'fr': {
+        'outOfRange': lambda breach_type: f'{breach_type} est en dehors de la plage!',
+        'temperature': 'Température',
+        'soc': 'État de charge',
+        'chargeRate': 'Taux de charge',
+        'batteryOk': 'Batterie est bonne',
+        'batteryNotOk': 'Batterie n\'est pas bonne'
+    },
+    'kn': {
+        'outOfRange': lambda breach_type: f'{breach_type} ವ್ಯಾಪ್ತಿಯಿಂದ ಹೊರಗಿದೆ ',
+        'temperature': 'ತಾಪಮಾನ',
+        'soc': 'ರಿಚಾರ್ಜ್ ಸ್ಥಿತಿ',
+        'chargeRate': 'ರಿಚಾರ್ಜ್ ದರ',
+        'batteryOk': 'ಬ್ಯಾಟರಿ ಸರಿಯಾಗಿದೆ',
+        'batteryNotOk': 'ಬ್ಯಾಟರಿ ಸರಿ ಇಲ್ಲ'
+
+    
+    }
+    
+    
+}
+
+
+
